1e847f70c7a31ee2a74b6116cb0827318346b32f
MohammedRoshanT <70361124+MohammedRoshanT@users.noreply.github.com>
2021-11-17 08:17:57 +0400

    String session

--- julie/StringSession.js
+++ julie/StringSession.js
@@ -11,7 +11,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('AMALSER_CODE' in process.env && string === undefined) {
+        if ('ROSHANSER_CODE' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -26,7 +26,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'Amalser;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'RoshanSer;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
